56d822f9c77668b0e0b70c4cb5453e0d3bb10f01
Delia Kossen <delia.kossen@students.fhnw.ch>
2022-03-08 12:26:38 +0100

    [change] week 2 ball exercise

--- week2/ball/ball.js
+++ week2/ball/ball.js
@@ -1,7 +1,8 @@
 
 const radius = 10;
-const ball = {x:20, y:0, dx: 5, dy: 1};
+const ball = {x:20, y:10, dx: 5, dy: 1};
 let   old  = {x: ball.x, y: ball.y};
+let callCount = 0;
 
 function start() {
     const canvas  = document.getElementById("canvas");
@@ -9,27 +10,46 @@ function start() {
     context.fillStyle = "black";
 
     setInterval(() => {
+        // stop if velocity is barely visible anymore
+        if (Math.abs(ball.dx) < 0.1 && Math.abs(ball.dy) < 0.1) {
+            return;
+        }
         nextBoard();
+        callCount++;
         display(context);
     }, 1000 / 20);
 }
 
 function nextBoard() {
     // keep old ball values for the sake of efficient clearing of the old display
-
+    old.x = ball.x;
+    old.y = ball.y;
     // handle ball is hitting the bounds
-    //   reverse direction
-    //   lose some energy relative to the current inertia (only velocity varies)
-
+    if (ball.y >= 390 && ball.dy > 0) {
+        // lose some energy relative to the current inertia (only velocity varies)
+        ball.dy -= 4;
+        // reverse direction
+        ball.dy *= -1;
+    } else if (ball.x <= 10 && ball.dx < 0 || ball.x >= 390 && ball.dx > 0) {
+        // reverse direction
+        ball.dx *= -1;
+        // lose some energy relative to the current inertia (only velocity varies)
+        ball.dx *= 0.8;
+    }
     // calculate new position
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    ball.y = Math.min(390, ball.y);
     // calculate any changes in velocity due to gravitational pull or medium resistance
+    ball.dy += 1.5;
+
 
 
 }
 
 function display(context) {
     context.clearRect(old.x - radius - 1 , old.y - radius -1 , 22, 22 );
-    fillBox(context)
+    fillBox(context);
 }
 
 function fillBox(context) {
