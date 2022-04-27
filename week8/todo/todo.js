8ed2c042efa38cc5b7b80927024a7827629c614d
Delia Kossen <delia.kossen@students.fhnw.ch>
2022-04-27 20:12:51 +0200

    [change] lesson 08 'todo'

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -3,20 +3,11 @@ function startTodo() {
 
 }
 
-let count = 0;
 function addTodo() {
-    const todoCount = count ++;
-
     const container = document.getElementById("todoContainer");
-    container.innerHTML += `
-    <tr>
-        <td>
-            <label for="todoText${todoCount}">First todo</label>
-            <input id="todoText${todoCount}" type="text"></td>
-        <td>
-            <label for="todoDone${todoCount}">First todo</label>
-            <input id="todoDone${todoCount}" type="checkbox"></td>
-    </tr>
-    `;
-    
+    container.innerHTML +=  `
+        <tr>
+            <td><input type="text"></td>
+        </tr>`;
+
 }
