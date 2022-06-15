089cc39efd5704a0c4aa5e4ad7b12698bbf57eae
Delia Kossen <delia.kossen@students.fhnw.ch>
2022-06-15 11:14:26 +0200

    week 14 crazy javascript notes, extra tests, qzs

--- week14/src/crazyTest.js
+++ week14/src/crazyTest.js
@@ -5,6 +5,14 @@ const crazySuite = TestSuite("crazy");
 
 crazySuite.add("equals", assert => {
 
+    assert.is( {} == NaN, false);
+    assert.is( true == 1, true);
+    assert.is( "" == false, true);
+    assert.is( 0 == false, true);
+    assert.is( [] == false, true);
+    assert.is( "0" == false, true);
+    assert.is( {} == false, false);
+
     const a = "0";
     const b =  0;
     const c = [];
@@ -12,6 +20,8 @@ crazySuite.add("equals", assert => {
     assert.is( a == b, true);
     assert.is( b == c, true);
     assert.is( a == c, false);
+    assert.is( a==!b, false);
+    assert.is( a>=b, true);
 });
 
 crazySuite.add("false", assert => {
@@ -25,11 +35,13 @@ crazySuite.add("false", assert => {
 
 crazySuite.add("coercion", assert => {
 
-    assert.is( "1"   == 1     ,true);
-    assert.is( +"2"  == 2     ,true);
-    assert.is( !"0"  == false ,true);
-    assert.is( "0"   == 0     ,true);
-    assert.is( !!"0" == true  ,true);
+    assert.is( "1"      == 1      ,true);
+    assert.is( +"2"     == 2      ,true);
+    assert.is( !0       == true   ,true);
+    assert.is( "0"      == 0      ,true);
+    assert.is( !"0"     == false  ,true);
+    assert.is( "0"      == 0      ,true);
+    assert.is( !!"0"    == true   ,true);
     assert.is  ( Number("0"), 0 );
 });
 
@@ -37,8 +49,8 @@ crazySuite.add("object", assert => {
 
     const coerce = x => x ? true : false ;
 
-    assert.is( coerce("0")            , true);
-    assert.is( coerce(+"0")           , false);
+    assert.is( coerce("0")               , true);
+    assert.is( coerce(+"0")              , false);
     assert.is( coerce(Number("0"))    , false);
     assert.is( coerce(new Number("0")), true);
 
@@ -46,7 +58,7 @@ crazySuite.add("object", assert => {
 
 crazySuite.add("refactor", assert => {
 
-    const x = 0; // fill here, try "0"
+    const x = 0; // fill here, try "0" or []
     //
     const if_1 = x => (x == true) ? true : false ;
     const if_2 = x =>  x          ? true : false ; // safe refactoring ???
@@ -59,6 +71,8 @@ crazySuite.add("refactor", assert => {
 crazySuite.add("other", assert => {
 
     assert.is( "2" + 1,      "21");
+    assert.is( +"2" + 1,      3);
+    assert.is( 1 + "2",      "12");
     assert.is( "2" - 1,      1);
     assert.is( "2" - - 1,    3);
     assert.is( 1 + 2 + "3",  "33");
@@ -71,7 +85,7 @@ crazySuite.add("other", assert => {
     assert.is( +[],          0);
     assert.is( 2 == [2],     true);
     assert.is( [] + {},      '[object Object]');
-    assert.is( {} + [],      '[object Object]'); // try in console
+    assert.is( {} + [],      '[object Object]'); // try in console -> 0 /--> ({} + []) -> "[object Object]"
 
 });
 
@@ -84,6 +98,7 @@ crazySuite.add("numbers", assert => {
     assert.is(typeof null ,             "object");
     assert.is(null instanceof Object ,  false);
     assert.is(typeof NaN ,              "number");
+    assert.is(typeof (+{}), "number");
     //
     assert.is(typeof (1/0) ,            "number");
     assert.is(0.1 + 0.2 === 0.3 ,       false);
@@ -103,7 +118,7 @@ crazySuite.add("compare", assert => {
 
     assert.is( {} == {} ,            false);
     assert.is( {} >  {} ,            false);
-    assert.is( {} >= {} ,            true);
+    assert.is( {} >= {} ,            true); // "not smaller than"
 
 });
 
